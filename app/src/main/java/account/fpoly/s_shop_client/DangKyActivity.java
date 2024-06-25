576b8ebb991f6512b3c0ec7d8c6fb45f77338371
bactv <bactvph26425@fpt.edu.vn>
2024-06-25 23:57:24 +0700

    bactvph26425 thao tac giao dien

--- app/src/main/java/account/fpoly/s_shop_client/DangKyActivity.java
+++ app/src/main/java/account/fpoly/s_shop_client/DangKyActivity.java
@@ -2,7 +2,10 @@ package account.fpoly.s_shop_client;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DangKyActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class DangKyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dang_ky);
+        ImageView backk=findViewById(R.id.back);
+        backk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(DangKyActivity.this, DangNhapActivity.class));
+            }
+        });
     }
 }
