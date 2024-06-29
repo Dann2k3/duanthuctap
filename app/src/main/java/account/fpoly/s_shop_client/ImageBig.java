5b3077efa5c0bcd901000aa2589570c0cd31d9ae
setboi <huyvxph27045@fpt.edu.vn>
2024-06-29 16:12:08 +0700

    huyvxph27045 giao dien

--- app/src/main/java/account/fpoly/s_shop_client/ImageBig.java
+++ app/src/main/java/account/fpoly/s_shop_client/ImageBig.java
@@ -1,40 +1,20 @@
 package account.fpoly.s_shop_client;
 
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.bumptech.glide.Glide;
 
-import account.fpoly.s_shop_client.API.API;
 
 public class ImageBig extends AppCompatActivity {
-    ImageView imageBig;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_big);
 
-
-        imageBig = findViewById(R.id.imageBig);
-        ImageView back = findViewById(R.id.back);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
-
-
-        SharedPreferences sharedPreferencesImage = getSharedPreferences("image", MODE_PRIVATE);
-        String link = sharedPreferencesImage.getString("link", null);
-
-        Glide.with(getBaseContext()).load(API.api_image + link)
-                .into(imageBig);
 
     }
 }
