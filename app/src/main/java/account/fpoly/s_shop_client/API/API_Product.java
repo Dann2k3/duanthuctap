c05abd797d6864a523621088472f63c7829c86e4
DT Quang <quangdtph21898@fpt.edu.vn>
2024-06-30 00:49:11 +0700

    quangdtph21898 api_product

---
+++ app/src/main/java/account/fpoly/s_shop_client/API/API_Product.java
@@ -0,0 +1,35 @@
+package account.fpoly.s_shop_client.API;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import account.fpoly.s_shop_client.Modal.ProductModal;
+import account.fpoly.s_shop_client.Modal.ReceProduct;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface API_Product {
+    Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyy").create();
+
+    API_Product apiProduct = new Retrofit.Builder()
+            .baseUrl(API.api)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(API_Product.class);
+    //    @FormUrlEncoded
+
+    @GET("product")
+    Call<ReceProduct> listProduct();
+    @GET("productHot")
+    Call<ReceProduct> listProductNew();
+    @GET("filterPrice")
+    Call<List<ProductModal>> filterProducts(@Query("minPrice") String minPrice, @Query("maxPrice") String maxPrice);
+
+    @GET("filterproduct")
+    Call<ReceProduct> filterName(@Query("name") String name);
+}
