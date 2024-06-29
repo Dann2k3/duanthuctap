e3920a40f1db3850492dbb8e2d9c0a500d106e79
DT Quang <quangdtph21898@fpt.edu.vn>
2024-06-30 00:27:29 +0700

    api_bill

---
+++ app/src/main/java/account/fpoly/s_shop_client/API/API_Bill.java
@@ -0,0 +1,46 @@
+package account.fpoly.s_shop_client.API;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import account.fpoly.s_shop_client.Modal.Bill;
+import account.fpoly.s_shop_client.Modal.BillMore;
+import account.fpoly.s_shop_client.Modal.ReceBillMores;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface API_Bill {
+
+
+    Gson gson = new GsonBuilder().setDateFormat("dd-MM-yyy").create();
+
+    API_Bill apiBill = new Retrofit.Builder()
+            .baseUrl(API.api)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(API_Bill.class);
+    //    @FormUrlEncoded
+
+    @POST("bill")
+    Call<Bill> addBill(@Body Bill bill);
+
+    @DELETE("bill/delete/{id}")
+    Call<BillMore> huyOder(@Path("id") String id);
+
+    @PUT("bill/update/{id}")
+    Call<Bill> updateBill(@Path("id") String id, @Body Bill bill);
+
+    @GET("billStatus")
+    Call<ReceBillMores> getListBillMores(@Query("id_user") String id_user, @Query("status") int status);
+
+    @PUT("bill/updateBillMores/{id}")
+    Call<BillMore> updateBillMores(@Path("id") String id, @Body BillMore billMore);
+}
