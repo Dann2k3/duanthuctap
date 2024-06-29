c72876345a73a7c12264efea5bd54376de8db316
bactv <bactvph26425@fpt.edu.vn>
2024-06-29 15:23:33 +0700

    bactvph26425 man hinh activity

--- app/src/main/java/account/fpoly/s_shop_client/Activity/DangNhapActivity.java
+++ app/src/main/java/account/fpoly/s_shop_client/Activity/DangNhapActivity.java
@@ -1,4 +1,4 @@
-package account.fpoly.s_shop_client;
+package account.fpoly.s_shop_client.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import account.fpoly.s_shop_client.R;
 
 public class DangNhapActivity extends AppCompatActivity {
     @Override
