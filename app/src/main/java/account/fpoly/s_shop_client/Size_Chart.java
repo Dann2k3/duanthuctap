2082ad0365cd85a35460877f9cf2ea7846436218
setboi <huyvxph27045@fpt.edu.vn>
2024-07-03 09:00:53 +0700

    huyvxph27045 chuc nang 1

--- app/src/main/java/account/fpoly/s_shop_client/Size_Chart.java
+++ app/src/main/java/account/fpoly/s_shop_client/Size_Chart.java
@@ -1,6 +1,8 @@
 package account.fpoly.s_shop_client;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,7 +13,12 @@ public class Size_Chart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_size_chart);
-
-
+        ImageView back = findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
     }
 }
